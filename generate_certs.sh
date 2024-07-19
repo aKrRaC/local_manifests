a81260740f8eac06e767ce03ce7ec411c5517345
aKrRaC <akhilrac2000@gmail.com>
2024-07-19 21:18:53 +0530

    dagda: Updated signing script

--- generate_certs.sh
+++ generate_certs.sh
@@ -3,23 +3,25 @@
 # Define the subject for the initial certificates
 subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=akhilrac2000@gmail.com'
 
-# Create the directory for the Android certificates
-mkdir -p ~/.android-certs
+# Print the subject line
+echo "Using Subject Line:"
+echo "$subject"
 
-# Generate initial certificates
-for cert in bluetooth cyngn-app media networkstack platform releasekey sdk_sandbox shared testcert testkey verity; do
-    ./development/tools/make_key ~/.android-certs/$cert "$subject"
+# Prompt the user to verify if the subject line is correct
+read -p "Is the subject line correct? (y/n): " confirmation
+
+# Check the user's response
+if [[ $confirmation != "y" && $confirmation != "Y" ]]; then
+    echo "Exiting without changes."
+    exit 1
+fi
+clear
+
+# Generate certificates
+for x in releasekey platform shared media networkstack verity otakey testkey cyngn-priv-app sdk_sandbox bluetooth verifiedboot nfc; do
+    ./development/tools/make_key vendor/awaken/signing/keys/$x "$subject"
 done
 
-# Copy the make_key tool to the certificates directory
-cp ./development/tools/make_key ~/.android-certs/
-
-# Modify the key size in the make_key tool from 2048 to 4096
-sed -i 's|2048|4096|g' ~/.android-certs/make_key
-
-# Generate APEX keys and convert to PEM format
-for apex in com.android.adbd com.android.adservices com.android.adservices.api com.android.appsearch com.android.art com.android.bluetooth com.android.btservices com.android.cellbroadcast com.android.compos com.android.configinfrastructure com.android.connectivity.resources com.android.conscrypt com.android.devicelock com.android.extservices com.android.graphics.pdf com.android.hardware.biometrics.face.virtual com.android.hardware.biometrics.fingerprint.virtual com.android.hardware.boot com.android.hardware.cas com.android.hardware.wifi com.android.healthfitness com.android.hotspot2.osulogin com.android.i18n com.android.ipsec com.android.media com.android.media.swcodec com.android.mediaprovider com.android.nearby.halfsheet com.android.networkstack.tethering com.android.neuralnetworks com.android.ondevicepersonalization com.android.os.statsd com.android.permission com.android.resolv com.android.rkpd com.android.runtime com.android.safetycenter.resources com.android.scheduling com.android.sdkext com.android.support.apexer com.android.telephony com.android.telephonymodules com.android.tethering com.android.tzdata com.android.uwb com.android.uwb.resources com.android.virt com.android.vndk.current com.android.vndk.current.on_vendor com.android.wifi com.android.wifi.dialog com.android.wifi.resources com.google.pixel.camera.hal com.google.pixel.vibrator.hal com.qorvo.uwb; do
-    subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN='$apex'/emailAddress=akhilrac2000@gmail.com'
-    ~/.android-certs/make_key ~/.android-certs/$apex "$subject"
-    openssl pkcs8 -in ~/.android-certs/$apex.pk8 -inform DER -nocrypt -out ~/.android-certs/$apex.pem
-done
+echo "Done! Now build as usual."
+echo "Make copies of your vendor/awaken/signing/keys folder as it contains your keys!"
+sleep 3
